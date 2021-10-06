318a168f839fb61af9e5ded2643ca47ba15ede7c
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-10-06 16:09:52 -0700

    home component

---
+++ src/components/home/home.js
@@ -0,0 +1,10 @@
+function Home () {
+    return (
+        <div className="Home">
+            <h1>FEW2.3 Single Page Web Applications</h1>
+            <h2>Danika Jaskulak-Gonzalez</h2>
+        </div>
+    )
+}
+
+export default Home
