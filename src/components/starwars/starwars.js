0b81b47ebe38add64da77563a37d87680f7fba42
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-10-06 17:22:52 -0700

    added more styling

--- src/components/starwars/starwars.js
+++ src/components/starwars/starwars.js
@@ -10,6 +10,7 @@ function StarWars () {
     const savedList = list.map(char => {
         return (
             <div className="savedCard">
+                <h1>Saved Characters</h1>
                 <div>
                     <h3>Character Details:</h3>
                     <div>Name: {char.name}</div>
@@ -44,38 +45,40 @@ function StarWars () {
                 >Enter</button>
             </div>
 
-            <div className="data">
-                {data && 
-                    <div>
+            <div className="content">
+                <div className="data">
+                    {data && 
                         <div>
-                            <h3>Character Details:</h3>
-                            <div>Name: {data.name}</div>
-                            <div>Height: {data.height}</div>
-                            <div>Mass: {data.mass}</div>
-                            <div>Hair Color: {data.hair_color}</div>
-                            <div>Eye Color: {data.eye_color}</div>
+                            <div>
+                                <h3>Character Details:</h3>
+                                <div>Name: {data.name}</div>
+                                <div>Height: {data.height}</div>
+                                <div>Mass: {data.mass}</div>
+                                <div>Hair Color: {data.hair_color}</div>
+                                <div>Eye Color: {data.eye_color}</div>
+                            </div>
+
+                            <div>
+                                <h3>Homeworld:</h3>
+                                <div>Planet: {data.homeworld.name}</div>
+                                <div>Terrain: {data.homeworld.terrain}</div>
+                                <div>Population: {data.homeworld.population}</div>
+                            </div>
+
+                            <button 
+                                onClick={e => {
+                                    setList([...list, data])
+                                }}
+                            >
+                                Save
+                            </button>
                         </div>
+                    }
+                </div>
 
-                        <div>
-                            <h3>Homeworld:</h3>
-                            <div>Planet: {data.homeworld.name}</div>
-                            <div>Terrain: {data.homeworld.terrain}</div>
-                            <div>Population: {data.homeworld.population}</div>
-                        </div>
-
-                        <button 
-                            onClick={e => {
-                                setList([...list, data])
-                            }}
-                        >
-                            Save
-                        </button>
-                    </div>
-                }
-            </div>
-
-            <div className="savedList">
-                {list && savedList}
+                <div className="savedList">
+                    {list && savedList}
+                </div>
             </div>
         </div>
     )
